0f6eea07965d215d4b616fa85afbd13f091c94e7
Elizar Abramov <nezha2077@mail.ru>
2021-11-06 20:21:14 +0700

    Build Success

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -10,9 +10,9 @@ class StatisticsServiceTest {
     void shouldfindMax() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = { 12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12 };
+        long[] incomesInBillions = { 12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12, 13 };
 
-        long expected = 12;
+        long expected = 13;
 
         long actual = service.findMax(incomesInBillions);
 
